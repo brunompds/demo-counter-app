78413eb6784fabfc31f39d49bb17acd4e0b153ff
brunompds <brunompds@gmail.com>
2022-11-03 14:53:39 +0000

    20221103_145339

--- giti.bat
+++ giti.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
+git commit -m "%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
 git push -u origin main
