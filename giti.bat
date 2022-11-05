4d2d7815d93fba633ef77dda612ae9bb7e2343a6
brunompds <brunompds@gmail.com>
2022-11-05 18:34:10 +0000

    20221105183410bruno_D:\DEVOPS2\GIT\Demoapplication\demo-counter-app

--- giti.bat
+++ giti.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "%cd%_%username%_%date:~-4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%"
+git commit -m "%date:~-4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%%username%_%cd%"
 git push -u origin main
