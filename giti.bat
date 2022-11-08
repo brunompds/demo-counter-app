c0eb8e144f92dab6d2e5acb0ff20407ce9857b95
brunompds <brunompds@gmail.com>
2022-11-08 17:11:55 +0000

    $message

--- giti.bat
+++ giti.bat
@@ -1,4 +1,6 @@
+SET message = "%date:~-4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%%username%_%cd%"
 git add .
-git commit -m "%date:~-4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%%username%_%cd%"
+IF NOT [%1]=="" message=%1
+git commit -m $message
+:message:~-4%%date:~
 git push -u origin main
-
