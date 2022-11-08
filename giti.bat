2474ce470b45252390610fcb1d9899199b373a81
brunompds <brunompds@gmail.com>
2022-11-08 17:23:00 +0000

    20221108172259bruno_D:\DEVOPS2\GIT\Demoapplication\demo-counter-app

--- giti.bat
+++ giti.bat
@@ -1,6 +1,8 @@
-SET message = "%date:~-4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%%username%_%cd%"
+
 git add .
-IF NOT [%1]=="" message=%1
-git commit -m $message
-:message:~-4%%date:~
+IF [%1]==[] ( 
+    git commit -m "%date:~-4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%%username%_%cd%"
+) ELSE (
+    git commit -m "%1"
+)
 git push -u origin main
